85ca8c46c83c20204674d03eb900f78a7ba26070
J <gG>
2022-10-01 20:12:26 +0800

    first commit

---
+++ docs/.vuepress/theme.ts
@@ -0,0 +1,89 @@
+import { hopeTheme } from "vuepress-theme-hope";
+import navbar from "./navbar.js";
+import sidebar from "./sidebar.js";
+
+export default hopeTheme({
+  hostname: "https://vuepress-theme-hope-v2-demo.mrhope.site",
+
+  author: {
+    name: "金洋学长",
+    url: "https://mrhope.site",
+  },
+  // iconAssets: "iconfont",
+  iconAssets: "//at.alicdn.com/t/c/font_3639341_ri400tyd1al.css",
+
+  logo: "/logo.png",
+
+  repo: "vuepress-theme-hope/vuepress-theme-hope",
+
+  docsDir: "demo/src",
+
+  // navbar
+  navbar: navbar,
+
+  // sidebar
+  sidebar: sidebar,
+
+  footer: "默认页脚",
+
+  displayFooter: true,
+
+  pageInfo: ["Author", "Original", "Date", "Category", "Tag", "ReadingTime"],
+
+  blog: {
+    
+    roundAvatar: true,
+    description: "00后程序猿｜后端攻城狮",
+    intro: "/intro.html",
+    medias: {
+      bilibili: "https://example.com",
+      Bitbucket: "https://example.com",
+      Dingding: "https://example.com",
+    },
+  },
+
+  encrypt: {
+    config: {
+      "/guide/encrypt.html": ["1234"],
+    },
+  },
+
+  plugins: {
+    blog: {
+      autoExcerpt: true,
+    },
+
+    // 如果你不需要评论，可以直接删除 comment 配置，
+    // 以下配置仅供体验，如果你需要评论，请自行配置并使用自己的环境，详见文档。
+    // 为了避免打扰主题开发者以及消耗他的资源，请不要在你的正式环境中直接使用下列配置!!!!!
+    comment: {
+      /**
+       * Using Giscus
+       */
+      provider: "Giscus",
+      repo: "vuepress-theme-hope/giscus-discussions",
+      repoId: "R_kgDOG_Pt2A",
+      category: "Announcements",
+      categoryId: "DIC_kwDOG_Pt2M4COD69",
+
+      /**
+       * Using Twikoo
+       */
+      // provider: "Twikoo",
+      // envId: "https://twikoo.ccknbc.vercel.app",
+
+      /**
+       * Using Waline
+       */
+      // provider: "Waline",
+      // serverURL: "https://vuepress-theme-hope-comment.vercel.app",
+    },
+
+    mdEnhance: {
+      enableAll: false,
+      presentation: {
+        plugins: ["highlight", "math", "search", "notes", "zoom"],
+      },
+    },
+  },
+});
